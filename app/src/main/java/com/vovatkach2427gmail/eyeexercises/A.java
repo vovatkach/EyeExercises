4cf154b82757b3463fb7edc41dda88a6efc82950
Volodymyr Tkach <vovatkach2427@gmail.com>
2017-03-25 16:53:05 +0200

    16:52

--- app/src/main/java/com/vovatkach2427gmail/eyeexercises/A.java
+++
@@ -1,8 +0,0 @@
-package com.vovatkach2427gmail.eyeexercises;
-
-/**
- * Created by vovat on 25.03.2017.
- */
-
-public class A {
-}
--- app/src/main/java/com/vovatkach2427gmail/eyeexercises/Act/PriewAct.java
+++ app/src/main/java/com/vovatkach2427gmail/eyeexercises/Act/PriewAct.java
@@ -1,15 +1,20 @@
 package com.vovatkach2427gmail.eyeexercises.Act;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
+import com.vovatkach2427gmail.eyeexercises.MainMenu;
 import com.vovatkach2427gmail.eyeexercises.R;
 
 public class PriewAct extends AppCompatActivity {
     CarouselView carouselView;
+    Button btnGoToMainMenu;
     int[] imgs={
             R.drawable.priew_act_carosel_img1,
             R.drawable.priew_act_carosel_img2,
@@ -20,12 +25,21 @@ public class PriewAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.priew_act);
+        btnGoToMainMenu=(Button)findViewById(R.id.btn_go_to_main_menu);
         carouselView=(CarouselView)findViewById(R.id.cvPriewAct);
         carouselView.setPageCount(imgs.length);
         carouselView.setImageListener(new ImageListener() {
             @Override
             public void setImageForPosition(int position, ImageView imageView) {
                 imageView.setImageResource(imgs[position]);
+            }
+        });
+        btnGoToMainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToMainMenu=new Intent(PriewAct.this,MainMenu.class);
+                startActivity(goToMainMenu);
+                overridePendingTransition(R.anim.in_left,R.anim.out_right);
             }
         });
     }
