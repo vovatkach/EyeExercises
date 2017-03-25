5b71a5b8ded01d67aefa177343f28a5a4ff69f22
Volodymyr Tkach <vovatkach2427@gmail.com>
2017-03-25 14:08:53 +0200

    create project

---
+++ app/src/main/java/com/vovatkach2427gmail/eyeexercises/WelcomeAct.java
@@ -0,0 +1,13 @@
+package com.vovatkach2427gmail.eyeexercises;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_act);
+    }
+}
